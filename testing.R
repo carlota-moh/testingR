6b584203dbc3b89fe1cf4565655c7f9a466fcc5e
carlota-moh <carlota.monederoh@gmail.com>
2021-02-04 17:41:50 +0100

    This is an exercise I did as part of my training

--- testing.R
+++ testing.R
@@ -1,2 +1,18 @@
-print('Hi my name is slim shady')
-5+3
+x <- 3
+y <- 5
+number <- x + y
+x <- 5
+number <- x+ y 
+glengths <- c(4.6, 300, 3000)
+glengths
+species <- c('e.coli', 'human', 'corn')
+combined <- c(glengths, species)
+combined
+expression <- c('low', 'high', 'medium', 'high', 'low', 'medium', 'high')
+expression <- factor(expression)
+samplegroup <- c('CTL', 'CTL', 'CTL', 'KO', 'KO', 'KO', 'OE', 'OE', 'OE')
+samplegroup <- factor(samplegroup)
+df <- data.frame(species, glengths)
+df
+list1 <- list(species, df, glengths)
+list1
