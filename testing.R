414fb86bec75bf4c1d28eee9f8d5d67d05c71b04
carlota-moh <carlota.monederoh@gmail.com>
2021-02-05 09:49:34 +0100

    New progress made!

--- testing.R
+++ testing.R
@@ -16,3 +16,27 @@ df <- data.frame(species, glengths)
 df
 list1 <- list(species, df, glengths)
 list1
+glengths <- c(glengths, 90)
+glenghts <- c(30, glengths)
+sqrt(81)
+sqrt(glengths)
+round(3.141592)
+?round
+ceiling(3.141592)
+args(round)
+example('round')
+?array
+example('array')
+mean(glengths)
+?mean
+mean(glengths, trim = 0.05)
+square_it <- function(x) {
+  square <- x * x
+  return(square)
+}
+square_it(2)
+sessionInfo()
+install.packages('ggplot2')
+install.packages('RTools')
+install.packages('BiocManager')
+#BiocManager::('name_of_package)
